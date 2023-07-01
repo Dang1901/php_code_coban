7ef1e6e55e6e06c44adc120e5685cb7582882841
Dang1901 <cudaunau19012003@gmail.com>
2023-07-01 15:21:43 +0700

    commit

---
+++ test1.php
@@ -0,0 +1,117 @@
+<?php
+   // $wel = 'chao mung cac ban';
+   // echo $wel;
+   
+   // 
+   
+   // $a = 2;
+   // $b = $a++;
+   // // $b = ++$a;
+
+   // echo 'a: '.$a.'<br/>';
+   // echo 'b: '.$b.'<br/>';
+
+   
+//    // toan tu +=
+//    $a = 10;
+//    $a+= 10;
+//    $a+= 20;
+//    echo $a;
+//    echo '<br/>';
+  
+//    // toan tu -=
+//    $a = 12;
+//    $a-= 20;
+//    $a-= 10;
+//    echo $a ;
+//    echo '<br/>';
+   
+//    // toan tu *=
+//    $a = 10;
+//    $a *= 20;
+//    $a *= 10;
+//    echo $a;
+//    echo '<br/>';
+
+//   // toan tu /=
+//    $a = 10;
+//    $a /= 20;
+//    $a /= 10;
+//    echo $a;
+//    echo '<br/>';
+
+//    // toan tu %
+//    $a = 10;
+//    $a%= 6;
+//    echo $a;
+//    echo '<br/>';
+
+//    $a = 10;
+//    $b = 11;
+//    $check =  $a>$b;
+//    var_dump($check);
+//    echo '<br/>';
+
+//    // toan tu === ( so sanh co gia tri va kieu du lieu)
+//    $a = 10;
+//    $b = 10; // chuoi
+//    $check = $a===$b;
+//    var_dump($check);
+//    echo '<br/>';
+
+//    // toan tu != 
+//    $a = 10;
+//    $b = 10; // chuoi
+//    $check = $a!=$b;
+//    var_dump($check);
+//    echo '<br/>';
+
+//    echo '<hr/>';
+//    /**
+//     * 1. hien thi so chan, so le trong day so tu: 1 2 3 4 5 ... 100
+//     *
+//     *
+//     */
+   
+//     $startIndex = 1;
+//     $endIndex = 100;
+
+//     $resultEven = null; // so chan
+//     $resultOdd = null; // so le
+
+//     $evenCount = 0; //Dem so chan
+//     $oddCount = 0; //Dem so le
+
+
+//      for ($index = $startIndex; $index <= $endIndex; $index++){
+//       // kiem tra so chan
+//       if ($index %2 == 0){
+//          // echo $index. 'la so chan <br/>';
+//          $resultEven.=$index.' ';
+//          $evenCount++;
+//      } else {
+//       // echo $index. 'la so le <br/>';
+//       $resultOdd.=$index.' ';
+//       $oddCount++;
+//      }
+//    }
+//  echo  'Tim thay '.$evenCount.' so chan:' .$resultEven. '<br/>';
+//  echo 'Tim thay  '.$oddCount. ' so le:' .$resultOdd. '<br/>';
+   
+   $a = 10.1;
+   $b = 12.1;
+   $c = ((int)($a)) + ((int)($b)) * 2;
+
+
+
+   if ($c > 0) {
+    echo 'May dung r';
+   } else {
+     echo 'may lai sai';
+   }
+  
+
+  
+
+
+?>
